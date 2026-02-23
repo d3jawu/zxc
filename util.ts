e2a5ee18f3ded43a3fab57da535e5216b4696739
Holly <holly@dejawu.me>
2026-02-22 23:55:24 -0800

    Color timer based on average and max runtimes

--- util.ts
+++ util.ts
@@ -1,4 +1,4 @@
-import chalk from "chalk";
+import chalk, { type ChalkInstance } from "chalk";
 import ansi from "ansi-escape-sequences";
 
 export const showError = (message: string) => {
@@ -6,18 +6,34 @@ export const showError = (message: string) => {
 };
 
 let spinnerId: NodeJS.Timeout | undefined;
+const runTimes: number[] = [];
+let maxRunTime = 0;
+let elapsed = 0;
 export const showTimer = () => {
   const startTime = Date.now();
-  process.stdout.write(`\n${ansi.style.gray}Model running  `);
+  const averageRunTime =
+    runTimes.reduce((val, sum) => val + sum, 0) / runTimes.length;
   spinnerId = setInterval(() => {
-    const elapsed = (Date.now() - startTime) / 1000;
+    elapsed = (Date.now() - startTime) / 1000;
+    let color: ChalkInstance;
+    if (elapsed <= averageRunTime) {
+      color = chalk.green;
+    } else if (elapsed <= maxRunTime) {
+      color = chalk.yellow;
+    } else {
+      color = chalk.red;
+    }
     process.stdout.write(
-      `${ansi.cursor.back(100)}Model running ${elapsed.toFixed(2)}s`,
+      `${ansi.cursor.back(100)}${chalk.gray("Model running")} ${color(elapsed.toFixed(2) + "s")}`,
     );
   }, 75);
 };
 
 export const hideTimer = () => {
+  if (elapsed > maxRunTime) {
+    maxRunTime = elapsed;
+  }
+  runTimes.push(elapsed);
   process.stdout.write(`${ansi.style.reset}\n`);
   clearInterval(spinnerId);
 };
